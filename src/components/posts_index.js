2fe34b9598b3d54334107986e712c31cc21890a1
raffaeleabramini <raffaele.abramini@gmail.com>
2017-01-15 16:15:38 +0000

    Add basic routing rules

---
+++ src/components/posts_index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class PostIndex extends Component {
+	render() {
+		return <ul>
+			<li>post 1</li>
+			<li>post 2</li>
+		</ul>
+	}
+}
+
+
+export default PostIndex;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 
-import App from './components/app';
+import routes from './routes';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
