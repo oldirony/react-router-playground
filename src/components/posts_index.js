d3b5fa218e2b452ce956bd0efab20b9d0cf20ace
oldirony <raffaele.abramini@gmail.com>
2017-01-29 14:08:48 +0000

    Map fetchPost action to PostIndex and invoke it on mount

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import { fetchPosts } from '../actions/';
 
 class PostIndex extends Component {
-	render() {
-		return <ul>
-			<li>post 1</li>
-			<li>post 2</li>
-		</ul>
-	}
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return <ul>
+            <li>post 1</li>
+            <li>post 2</li>
+        </ul>
+    }
 }
 
-
-export default PostIndex;
+export default connect(null, { fetchPosts })(PostIndex);
