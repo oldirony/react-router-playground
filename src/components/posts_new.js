2b19e5222ef024b9ea890d94cfb0a260bb60d475
oldirony <raffaele.abramini@gmail.com>
2017-01-29 16:02:15 +0000

    PostsNew component
    - add component
    - setup route

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class PostsNew extends Component {
+    render() {
+        return <div>
+            new post form
+        </div>
+    }
+}
+
+
+export default connect(null, null)(PostsNew);
